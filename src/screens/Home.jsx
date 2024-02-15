1ec2b68c9c1ce9b6678a2774da11a3c9e21b6784
emilianovallejos1986 <emilianovallejos1986@gmail.com>
2024-02-14 23:52:38 -0300

    commit lV

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -14,8 +14,8 @@ export default Home;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      width: "100%",
-      alignItems: "center",
+    flex: 1,
+    width: "100%",
+    alignItems: "center",
     },
-  });
+});
